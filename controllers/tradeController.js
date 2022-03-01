54e30a43176df42a6f1c3670183d3b96ab55ea0b
krishna-v98 <krishnavangipurapu@gmail.com>
2022-03-01 17:36:49 -0500

    CRUD implemented

--- controllers/tradeController.js
+++ controllers/tradeController.js
@@ -3,7 +3,13 @@ const model = require('../models/items');
 //show all
 exports.index = (req, res) => {
     let trades = model.find();
-    res.render('./trade/index', { trade: trades });
+    let names = [];
+    for (let i = 0; i < trades.length; i++) {
+        names.push(trades[i].category);
+    }
+    let uniqueNames = [...new Set(names)];
+    res.render('./trade/index', { trades: trades, names: uniqueNames });
+    console.log(uniqueNames);
 };
 
 exports.new = (req, res) => {
@@ -19,4 +25,37 @@ exports.show = (req, res) => {
     else {
         res.status(404).send('Item not found');
     }
+};
+
+exports.create = (req, res) => {
+    let trade = req.body;
+    model.save(trade);
+    res.redirect('/trades');
+};
+
+exports.edit = (req, res) => {
+    let id = req.params.id;
+    let trade = model.findById(id);
+    if (trade) {
+        res.render('./trade/edit', { trade });
+    }
+    else
+        res.status(404).send('item not found');
+};
+
+exports.update = (req, res) => {
+    let trade = req.body;
+    let id = req.params.id;
+    if (model.updateById(id, trade)) {
+        res.redirect('/trades/' + id);
+    } else
+        res.status(404).send('can\'t edit');
+};
+
+exports.delete = (req, res) => {
+    let id = req.params.id;
+    if (model.deleteById(id)) {
+        res.redirect('/trades');
+    }
+    else res.status(404).send('404 item not found');
 };
--- models/items.js
+++ models/items.js
@@ -1,76 +1,104 @@
 const { DateTime } = require('luxon');
 const { v4: uuidv4 } = require('uuid');
 
-const trades =
-{
-    Laptops: [
-        {
-            id: 'a1',
-            createdAt: DateTime.now().toLocaleString(DateTime.DATETIME_MED),
-            name: 'ROG Zephyrus G15',
-            price: '$1800',
-            description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt distinctio optio consequuntur voluptates, natus nemo tempora odit et quam at placeat suscipit corporis ullam adipisci iure maxime explicabo minus! Dolorem voluptas ab minima sapiente quidem sit perferendis, officiis culpa voluptates iusto numquam qui voluptatibus ea eum amet itaque, quod temporibus odit explicabo dolores tempora ad. Nam error aut odit perspiciatis.'
-        },
-        {
-            id: 'a2',
-            createdAt: DateTime.now().toLocaleString(DateTime.DATETIME_MED),
-            name: 'Apple Macbook Pro M1',
-            price: '$2000',
-            description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt distinctio optio consequuntur voluptates, natus nemo tempora odit et quam at placeat suscipit corporis ullam adipisci iure maxime explicabo minus! Dolorem voluptas ab minima sapiente quidem sit perferendis, officiis culpa voluptates iusto numquam qui voluptatibus ea eum amet itaque, quod temporibus odit explicabo dolores tempora ad. Nam error aut odit perspiciatis.'
-        },
-        {
-            id: 'a3',
-            createdAt: DateTime.now().toLocaleString(DateTime.DATETIME_MED),
-            name: 'Apple Macbook Pro M1 Max',
-            price: '$2500',
-            description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt distinctio optio consequuntur voluptates, natus nemo tempora odit et quam at placeat suscipit corporis ullam adipisci iure maxime explicabo minus! Dolorem voluptas ab minima sapiente quidem sit perferendis, officiis culpa voluptates iusto numquam qui voluptatibus ea eum amet itaque, quod temporibus odit explicabo dolores tempora ad. Nam error aut odit perspiciatis.'
-        }
-    ],
-    Mobiles: [
-        {
-            id: 'b1',
-            createdAt: DateTime.now().toLocaleString(DateTime.DATETIME_MED),
-            name: 'iPhone 13 Pro Max',
-            price: '$1300',
-            description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt distinctio optio consequuntur voluptates, natus nemo tempora odit et quam at placeat suscipit corporis ullam adipisci iure maxime explicabo minus! Dolorem voluptas ab minima sapiente quidem sit perferendis, officiis culpa voluptates iusto numquam qui voluptatibus ea eum amet itaque, quod temporibus odit explicabo dolores tempora ad. Nam error aut odit perspiciatis.'
-        },
-        {
-            id: 'b2',
-            createdAt: DateTime.now().toLocaleString(DateTime.DATETIME_MED),
-            name: 'iPhone 13 Pro ',
-            price: '$1200',
-            description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt distinctio optio consequuntur voluptates, natus nemo tempora odit et quam at placeat suscipit corporis ullam adipisci iure maxime explicabo minus! Dolorem voluptas ab minima sapiente quidem sit perferendis, officiis culpa voluptates iusto numquam qui voluptatibus ea eum amet itaque, quod temporibus odit explicabo dolores tempora ad. Nam error aut odit perspiciatis.'
-        }
-    ],
-    Sound: [
-        {
-            id: 'c1',
-            createdAt: DateTime.now().toLocaleString(DateTime.DATETIME_MED),
-            name: 'JBL Charge 5',
-            price: '$180',
-            description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt distinctio optio consequuntur voluptates, natus nemo tempora odit et quam at placeat suscipit corporis ullam adipisci iure maxime explicabo minus! Dolorem voluptas ab minima sapiente quidem sit perferendis, officiis culpa voluptates iusto numquam qui voluptatibus ea eum amet itaque, quod temporibus odit explicabo dolores tempora ad. Nam error aut odit perspiciatis.'
-        },
-        {
-            id: 'c2',
-            createdAt: DateTime.now().toLocaleString(DateTime.DATETIME_MED),
-            name: 'Bose Soundbar ',
-            price: '$800',
-            description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt distinctio optio consequuntur voluptates, natus nemo tempora odit et quam at placeat suscipit corporis ullam adipisci iure maxime explicabo minus! Dolorem voluptas ab minima sapiente quidem sit perferendis, officiis culpa voluptates iusto numquam qui voluptatibus ea eum amet itaque, quod temporibus odit explicabo dolores tempora ad. Nam error aut odit perspiciatis.'
-        }
-    ]
-};
+const trades = [
+    {
+        id: 'a1',
+        createdAt: DateTime.now().toLocaleString(DateTime.DATETIME_MED),
+        name: 'ROG Zephyrus G15',
+        price: '$1800',
+        description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt distinctio optio consequuntur voluptates, natus nemo tempora odit et quam at placeat suscipit corporis ullam adipisci iure maxime explicabo minus! Dolorem voluptas ab minima sapiente quidem sit perferendis, officiis culpa voluptates iusto numquam qui voluptatibus ea eum amet itaque, quod temporibus odit explicabo dolores tempora ad. Nam error aut odit perspiciatis.',
+        imageLink: 'https://rog.asus.com/media/1578098363696.jpg',
+        category: 'Laptops'
+    },
+    {
+        id: 'a2',
+        createdAt: DateTime.now().toLocaleString(DateTime.DATETIME_MED),
+        name: 'Apple Macbook Pro M1',
+        price: '$2000',
+        description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt distinctio optio consequuntur voluptates, natus nemo tempora odit et quam at placeat suscipit corporis ullam adipisci iure maxime explicabo minus! Dolorem voluptas ab minima sapiente quidem sit perferendis, officiis culpa voluptates iusto numquam qui voluptatibus ea eum amet itaque, quod temporibus odit explicabo dolores tempora ad. Nam error aut odit perspiciatis.',
+        imageLink: 'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mbp16-spacegray-gallery1-202110?wid=4000&hei=3072&fmt=jpeg&qlt=80&.v=1632799176000',
+        category: 'Laptops'
+    },
+    {
+        id: 'a3',
+        createdAt: DateTime.now().toLocaleString(DateTime.DATETIME_MED),
+        name: 'Apple Macbook Pro M1 Max',
+        price: '$2500',
+        description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt distinctio optio consequuntur voluptates, natus nemo tempora odit et quam at placeat suscipit corporis ullam adipisci iure maxime explicabo minus! Dolorem voluptas ab minima sapiente quidem sit perferendis, officiis culpa voluptates iusto numquam qui voluptatibus ea eum amet itaque, quod temporibus odit explicabo dolores tempora ad. Nam error aut odit perspiciatis.',
+        imageLink: 'https://www.bhphotovideo.com/images/images2500x2500/apple_mbp_16_sg_25_16_2_macbook_pro_with_1668323.jpg',
+        category: 'Laptops'
+    },
+    {
+        id: 'b1',
+        createdAt: DateTime.now().toLocaleString(DateTime.DATETIME_MED),
+        name: 'iPhone 13 Pro Max',
+        price: '$1300',
+        description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt distinctio optio consequuntur voluptates, natus nemo tempora odit et quam at placeat suscipit corporis ullam adipisci iure maxime explicabo minus! Dolorem voluptas ab minima sapiente quidem sit perferendis, officiis culpa voluptates iusto numquam qui voluptatibus ea eum amet itaque, quod temporibus odit explicabo dolores tempora ad. Nam error aut odit perspiciatis.',
+        imageLink: 'https://rog.asus.com/media/1578098363696.jpg',
+        category: 'Mobiles'
+    },
+    {
+        id: 'b2',
+        createdAt: DateTime.now().toLocaleString(DateTime.DATETIME_MED),
+        name: 'iPhone 13 Pro ',
+        price: '$1200',
+        description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt distinctio optio consequuntur voluptates, natus nemo tempora odit et quam at placeat suscipit corporis ullam adipisci iure maxime explicabo minus! Dolorem voluptas ab minima sapiente quidem sit perferendis, officiis culpa voluptates iusto numquam qui voluptatibus ea eum amet itaque, quod temporibus odit explicabo dolores tempora ad. Nam error aut odit perspiciatis.',
+        imageLink: 'https://rog.asus.com/media/1578098363696.jpg',
+        category: 'Mobiles',
+    },
+    {
+        id: 'c1',
+        createdAt: DateTime.now().toLocaleString(DateTime.DATETIME_MED),
+        name: 'JBL Charge 5',
+        price: '$180',
+        description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt distinctio optio consequuntur voluptates, natus nemo tempora odit et quam at placeat suscipit corporis ullam adipisci iure maxime explicabo minus! Dolorem voluptas ab minima sapiente quidem sit perferendis, officiis culpa voluptates iusto numquam qui voluptatibus ea eum amet itaque, quod temporibus odit explicabo dolores tempora ad. Nam error aut odit perspiciatis.',
+        imageLink: 'https://rog.asus.com/media/1578098363696.jpg',
+        category: 'Sound',
+    },
+    {
+        id: 'c2',
+        createdAt: DateTime.now().toLocaleString(DateTime.DATETIME_MED),
+        name: 'Bose Soundbar ',
+        price: '$800',
+        description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt distinctio optio consequuntur voluptates, natus nemo tempora odit et quam at placeat suscipit corporis ullam adipisci iure maxime explicabo minus! Dolorem voluptas ab minima sapiente quidem sit perferendis, officiis culpa voluptates iusto numquam qui voluptatibus ea eum amet itaque, quod temporibus odit explicabo dolores tempora ad. Nam error aut odit perspiciatis.',
+        imageLink: 'https://rog.asus.com/media/1578098363696.jpg',
+        category: 'Sound',
+    },
+];
 
 let names = Object.keys(trades);
 
 exports.find = () => trades;
 
-exports.findById = id => {
-    for (let i = 0; i < names.length; i++) {
-        for (let j = 0; j < trades[names[i]].length; j++) {
-            if(trades[names[i]][j].id == id){
-                return trades[names[i]][j];
-            }
-        }
-    }
-    return -1;
+exports.findById = id => trades.find(trade => trade.id === id);
+
+exports.save = trade => {
+    trade.id = uuidv4();
+    trade.createdAt = DateTime.now().toLocaleString(DateTime.DATETIME_SHORT);
+    trades.push(trade);
+    console.log(trades);
 };
+
+exports.updateById = (id, newTrade) => {
+    let trade = trades.find(trade => trade.id === id);
+    if (trade) {
+        trade.name = newTrade.name;
+        trade.price = newTrade.price;
+        trade.imageLink = newTrade.imageLink;
+        trade.description = newTrade.description;
+        trade.category = newTrade.category;
+        return true;
+    }
+    else return false;
+}
+
+
+exports.deleteById = id => {
+    let index = trades.findIndex(trade => trade.id === id);
+    if (index != -1) {
+        trades.splice(index, 1);
+        return true;
+    }
+    else return false;
+}
